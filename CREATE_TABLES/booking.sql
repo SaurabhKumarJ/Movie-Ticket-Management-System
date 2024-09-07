50c6d116435cddbc4f005469215828f1322bd7ff
Saurabh Kumar Jyoti <149100538+SaurabhKumarJ@users.noreply.github.com>
2024-09-07 21:52:45 +0530

    Formating booking.sql

--- CREATE_TABLES/booking.sql
+++ CREATE_TABLES/booking.sql
@@ -6,8 +6,10 @@ Card_Number varchar(19),
 Name_on_card varchar(21),
 User_ID varchar(5),
 Show_ID varchar(10),
-Foreign Key (User_ID) REFERENCES Web_User (Web_User_ID) ON
-DELETE CASCADE ON UPDATE CASCADE,
-Foreign Key (Show_ID) REFERENCES Shows(Show_ID) ON DELETE
-CASCADE ON UPDATE CASCADE,
+Foreign Key (User_ID) REFERENCES Web_User (Web_User_ID) 
+  ON DELETE CASCADE 
+  ON UPDATE CASCADE,
+Foreign Key (Show_ID) REFERENCES Shows(Show_ID) 
+  ON DELETE CASCADE 
+  ON UPDATE CASCADE,
 Primary Key(Booking_ID));
